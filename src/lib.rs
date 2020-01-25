f1165e61ca0d80737d697fe7136f818b2b5bbbc3
notrodes <reallystupidid@gmail.com>
2020-01-25 18:25:12 -0500

    Used `match` not `if`

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
 #[cfg(test)]
 mod tests {
     use crate::cereal_simulation::simulation;
-    //    #[test]
-    //    fn run_simple() {
-    //        let loop_numbers = [1, 10, 100];
-    //        println!("Single-threaded:");
-    //        for number_of_loops in loop_numbers.iter() {
-    //            println!("Number of Simulations:{}", number_of_loops);
-    //            let (max, min, mean, length) = simulation(*number_of_loops, false);
-    //            println!(
-    //                "Max:{} Min:{} Mean:{} Length:{:?}\n",
-    //                max.unwrap(),
-    //                min.unwrap(),
-    //                mean.unwrap(),
-    //                length.unwrap()
-    //            );
-    //        }
-    //    }
+    #[test]
+    fn run_simple() {
+        let loop_numbers = [1, 10, 100];
+        println!("Single-threaded:");
+        for number_of_loops in loop_numbers.iter() {
+            println!("Number of Simulations:{}", number_of_loops);
+            let (max, min, mean, length) = simulation(*number_of_loops, 1);
+            println!(
+                "Max:{} Min:{} Mean:{} Length:{:?}\n",
+                max.unwrap(),
+                min.unwrap(),
+                mean.unwrap(),
+                length.unwrap()
+            );
+        }
+    }
     #[test]
     fn run_multi() {
-        let loop_numbers = [10, 100];
+        let loop_numbers = [1, 10, 100];
         println!("Multi-threaded:");
         for number_of_loops in loop_numbers.iter() {
             println!("Number of Simulations:{}", number_of_loops);
-            let (max, min, mean, length) = simulation(*number_of_loops, true);
+            let (max, min, mean, length) = simulation(*number_of_loops, 4);
             println!(
                 "Max:{} Min:{} Mean:{} Length:{:?}\n",
                 max.unwrap(),
@@ -48,92 +48,79 @@ pub mod cereal_simulation {
         number_of_loops: i32,
         sender: Option<Sender<Option<Vec<i32>>>>,
     ) -> Option<Vec<i32>> {
-        let sender_clone = sender.clone();
-        let multi_core;
-        match sender_clone {
-            Some(_t) => {
-                multi_core = true;
+        match number_of_loops {
+            0 => {
+                match sender {
+                    None => sender.unwrap().send(None).unwrap(),
+                    _ => (),
+                }
+                return None;
             }
-            None => {
-                multi_core = false;
+            _ => {
+                let mut probability: Vec<i32> = Vec::new();
+                // Simulation loop
+                for _i in 0..number_of_loops {
+                    let mut prizes: [i32; 6] = [0, 0, 0, 0, 0, 0];
+                    let mut opens: i32 = 0;
+                    // loop until owns every prize
+                    while prizes.contains(&0) {
+                        prizes[thread_rng().gen_range(0, 6)] += 1;
+                        opens += 1;
+                    }
+                    probability.push(opens);
+                }
+                match sender.clone() {
+                    Some(_t) => {
+                        sender.unwrap().send(Some(probability)).unwrap();
+                        None
+                    }
+                    None => Some(probability),
+                }
             }
-        }
-        if number_of_loops == 0 {
-            if multi_core {
-                sender.unwrap().send(None).unwrap();
-            }
-            return None;
-        }
-        let mut probability: Vec<i32> = Vec::new();
-        // Simulation loop
-        for _i in 0..number_of_loops {
-            let mut prizes: [i32; 6] = [0, 0, 0, 0, 0, 0];
-            let mut opens: i32 = 0;
-            // loop until owns every prize
-            while prizes.contains(&0) {
-                prizes[thread_rng().gen_range(0, 6)] += 1;
-                opens += 1;
-            }
-            probability.push(opens);
-        }
-        if multi_core {
-            sender.unwrap().send(Some(probability)).unwrap();
-            None
-        } else {
-            Some(probability)
         }
     }
 
     pub fn simulation(
         number_of_loops: i32,
-        concurrent: bool,
+        number_of_threads: i32,
     ) -> (Option<i32>, Option<i32>, Option<f64>, Option<Duration>) {
         // Avoid divide by zero errors with this check
-        if number_of_loops == 0 {
-            (None, None, None, None)
-        } else {
-            // Start timer
-            let start = Instant::now();
-            // Allocate memory for variables
-            let mut probability: Vec<i32>;
-            if concurrent {
-                // Number of threads to spawn
-                const NUMBER_OF_THREADS: i32 = 4;
-                let child_load = (number_of_loops as f32 / NUMBER_OF_THREADS as f32).floor() as i32;
+        match number_of_loops {
+            0 => (None, None, None, None),
+            _ => {
+                // Start timer
+                let start = Instant::now();
+                let child_load = (number_of_loops as f32 / number_of_threads as f32).floor() as i32;
                 // Create channel
                 let (tx, rx) = mpsc::channel();
-                for _i in 0..NUMBER_OF_THREADS {
+                for _i in 0..(number_of_threads - 1) {
                     // Create a sender and a number of loop for the other thread
-                    let tx = mpsc::Sender::clone(&tx);
+                    let tx_clone = mpsc::Sender::clone(&tx);
                     let child_load_clone = child_load.clone();
                     // Spawn thread for calculations
-                    thread::spawn(move || calculate(child_load_clone, Some(tx)));
+                    thread::spawn(move || calculate(child_load_clone, Some(tx_clone)));
                 }
-                let load = (number_of_loops as f32 / NUMBER_OF_THREADS as f32).ceil() as i32;
-                probability = calculate(load, None).unwrap();
+                let load = (number_of_loops as f32 / number_of_threads as f32).ceil() as i32;
+                let mut probability = calculate(load, None).unwrap();
                 for data in rx {
                     match data {
-                        Some(mut T) => {
-                            probability.append(&mut T);
+                        Some(mut t) => {
+                            probability.append(&mut t);
                         }
                         None => probability.append(&mut vec![0]),
                     }
                 }
-            } else if !concurrent {
-                probability = calculate(number_of_loops, None).unwrap();
                 probability.shrink_to_fit();
-            } else {
-                panic!("Data race? in Rust?");
+                // End timer
+                let timer = Some(start.elapsed());
+                println!("{}", *probability.iter().max().unwrap());
+                (
+                    Some(*probability.iter().max().unwrap()),
+                    Some(*probability.iter().min().unwrap()),
+                    Some(probability.iter().sum::<i32>() as f64 / probability.len() as f64),
+                    timer,
+                )
             }
-            // End timer
-            let timer = Some(start.elapsed());
-            println!("{}", *probability.iter().max().unwrap());
-            (
-                Some(*probability.iter().max().unwrap()),
-                Some(*probability.iter().min().unwrap()),
-                Some(probability.iter().sum::<i32>() as f64 / probability.len() as f64),
-                timer,
-            )
         }
     }
 }
